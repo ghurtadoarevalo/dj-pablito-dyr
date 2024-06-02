2f13c133f674e25862690362f26b65d7215f9cd5
Gustavo H <gustavo.hurtado@usach.cl>
2024-06-02 14:36:49 -0400

    ADD: seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   server: {
-    host: false
+    host: true
   },
   site: 'https://djpablitodyr.cl',
   integrations: [react(), tailwind()],
